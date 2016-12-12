17b2d7e787a17d811a14aabc497f77e20a8fd1ca
thanhhuynhsg <huynhcongthanh.sg@gmail.com>
2016-12-12 12:09:06 +0700

    Morning 12/12/2016
    cập nhật thêm được giao diện trang admin, database đang trong quá trình
    sửa chữa

---
+++ SaGoFit/Areas/Adminstrator/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SaGoFit.Areas.Adminstrator.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Adminstrator/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
